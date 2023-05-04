be0e73ff2399475b407cfaf04f58615a3ddca653
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-05-04 13:32:05 +0100

    trying again images bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b0e8b87d6be757c"; }
+/******/ 		__webpack_require__.h = function() { return "a69552bfccbaf8eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/icons.tsx
+++ components/icons.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image'
 
 export function IconBottle({className}:any) {
     return (
-        <Image className={className} src="../mama-birthday-website/public/1.svg" alt="bottleSVG" width={100} height={100} />
+        <Image className={className} src="public/1.svg" alt="bottleSVG" width={100} height={100} />
     )
 }
 
 export function FlowerOverlay({className}:any, {id}:any) {
     return (
-        <img className={className} src=".../mama-birthday-website/public/3.png" alt="flowers" id={id} />
+        <img className={className} src="public/3.png" alt="flowers" id={id} />
     )
 }
 
 export function Wave({className}:any) {
     return (
-        <img className={className} src="assets/wavy.svg" alt="wave" />
+        <img className={className} src="public/wavy.svg" alt="wave" />
     )
 }
 
 export function Shoes({className}:any) {
     return (
-        <img className={className} src="/shoes.png" alt="shoes with pink feathers" />
+        <img className={className} src="public/shoes.png" alt="shoes with pink feathers" />
     )
 }
